7bb5e404387afe80effbc6860db034cc9ad35b11
HeartieSelas <151001388+HeartieSelas@users.noreply.github.com>
2024-01-11 14:50:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Navbar from "./Components/Nav";
+import Card from "./Components/Card";
+
+function App(){
+  return(
+    <React.Fragment>
+      <Navbar/>
+      <Card/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,64 @@
+
+import React from 'react';
+
+
+const App=()=>{
+  return(
+    <>
+
+    <h1 className='text-center text-success my-5'>Bootstrap Cards</h1>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-3'>
+        <div class="card">
+  <img src="https://images.pexels.com/photos/707344/pexels-photo-707344.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Nature 1</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Read More</a>
+  </div>
+</div>
+        </div>
+
+        <div className='col-md-3'>
+        <div class="card">
+  <img src="https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Nature 2</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Read More</a>
+  </div>
+</div>
+        </div>
+
+        <div className='col-md-3'>
+        <div class="card">
+  <img src="https://images.pexels.com/photos/101529/pexels-photo-101529.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Nature 3</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Read More</a>
+  </div>
+</div>
+        </div>
+
+        <div className='col-md-3'>
+        <div class="card">
+  <img src="https://images.pexels.com/photos/228095/pexels-photo-228095.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Nature 4</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Read More</a>
+  </div>
+</div>
+        </div>
+
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default App;
+
